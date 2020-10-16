2a334fb868b28c310372d3d7c33a40cddfb3b9e7
boning <boning@ucsb.edu>
2020-10-16 12:20:28 -0700

    Added usage information and cid parser in the main function.

--- client.cpp
+++ client.cpp
@@ -15,7 +15,28 @@
 #include <sys/time.h>
 #include <thread>
 
+const char* usage = "Run the program by typing ./client <cid> where cid is within range [0, 2].";
+inline void print_usage() {
+    printf("%s\n", usage);
+}
+
 int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        print_usage();
+        exit(1);
+    }
+    
+    int cid = atoi(argv[1]);
+    if (cid < 0 || cid > 2) {
+        std::cout << "Your input cid is out of the accepted range." << std::endl;
+        print_usage();
+        exit(1);
+    }
+
+    client c(cid);
+    
+
+    // Test Code
     timespec t0, t1, diff, div;
     clock_gettime(CLOCK_REALTIME, &t0);
     while(true) {
